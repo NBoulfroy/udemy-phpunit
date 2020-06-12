239f60f733c642a7ee6a152df0b7347e8b56ad09
BOULFROY Nicolas <nicolas.boulfroy@hotmail.fr>
2020-06-12 16:13:31 +0200

    functions file updated

---
+++ src/Functions.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Function library.
+ *
+ * @Project : phpunit
+ * @File    : Functions.php
+ * @Author  : BOULFROY Nicolas
+ * @Create  : 2020/05/22
+ * @Create  : 2020/06/12
+ */
+
+namespace Src;
+
+class Functions
+{
+    /**
+     * @param int $a First number
+     * @param int $b Second number
+     *
+     * @return int
+     */
+    public static function add(int $a, int $b): int
+    {
+        return $a + $b;
+    }
+}
