ff03655599dd415c60a5699ba13f5c89559c754d
BOULFROY Nicolas <nicolas.boulfroy@hotmail.fr>
2020-06-12 16:14:35 +0200

    UserTest updated
    Code compliance

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -6,14 +6,39 @@
  * @File    : UserTest.php
  * @Author  : BOULFROY Nicolas
  * @Create  : 2020/05/22
+ * @Update  : 2020/06/12
  */
 
 namespace Tests;
 
-use User;
+use Src\User;
 use PHPUnit\Framework\TestCase;
+
+require 'vendor/autoload.php';
 
 class UserTest extends TestCase
 {
+    /**
+     * Test function with attributes setting.
+     */
+    public function testReturnsFullName()
+    {
+        $user = new User();
+        $user->firstName = 'Nicolas';
+        $user->lastName = 'BOULFROY';
 
+        $this->assertEquals('Nicolas BOULFROY', $user->getFullName());
+    }
+
+    /**
+     * Test function with no attributes setting.
+     *
+     * @test
+     */
+    public function full_name_isEmptyByDefault()
+    {
+        $user = new User();
+
+        $this->assertEquals('', $user->getFullName());
+    }
 }
