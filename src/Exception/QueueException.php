58c6a9ca988462eece798a387f05057a2da7d8a4
BOULFROY Nicolas <nicolas.boulfroy@hotmail.fr>
2020-06-19 17:34:44 +0200

    QueueException added
    Fully implemented

---
+++ src/Exception/QueueException.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * QueueException class.
+ *
+ * @Project : udemy-phpunit
+ * @File    : QueueException.php
+ * @Author  : BOULFROY Nicolas
+ * @Create  : 2020/06/19
+ */
+
+namespace Src\Exception;
+
+use \Exception;
+
+class QueueException extends Exception
+{
+    /**
+     * QueueException constructor.
+     *
+     * @param string         $message
+     * @param int            $code
+     * @param Exception|null $previous
+     */
+    public function __construct(string $message = 'Queue is full', int $code = 500, Exception $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+}
