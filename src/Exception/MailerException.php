b0028d708d9bc1bacdd6e0466f2653d7756e6be4
BOULFROY Nicolas <nicolas.boulfroy@hotmail.fr>
2020-06-26 17:51:10 +0200

    MailerException added
    Fully implemented

---
+++ src/Exception/MailerException.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * MailerException class.
+ *
+ * @Project : udemy-phpunit
+ * @File    : MailerException.php
+ * @Author  : BOULFROY Nicolas
+ * @Create  : 2020/06/26
+ * @Update  :
+ */
+
+namespace Exception;
+
+use \Exception;
+
+class MailerException extends Exception
+{
+    /**
+     * MailerException constructor.
+     *
+     * @param string         $message
+     * @param int            $code
+     * @param Exception|null $previous
+     */
+    public function __construct(string $message = 'No email inform', int $code = 500, Exception $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+}
