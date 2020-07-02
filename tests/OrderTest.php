9726399673dbf66a2c3c4e939773fc1e7c740192
BOULFROY Nicolas <nicolas.boulfroy@hotmail.fr>
2020-07-02 12:24:15 +0200

    OrderTest updated
    Using Mockery to mock a dependency that doesn't exist yet (tearDown & testOrderIsProcessedUsingMockery methods added)

--- tests/OrderTest.php
+++ tests/OrderTest.php
@@ -6,18 +6,24 @@
  * @File    : OrderTest.php
  * @Author  : BOULFROY Nicolas
  * @Create  : 2020/06/30
- * @Update  : 2020/07/01
+ * @Update  : 2020/07/02
  */
 
 namespace Tests;
 
 use PHPUnit\Framework\TestCase;
 use Order;
+use \Mockery;
 
 require 'vendor/autoload.php';
 
 class OrderTest extends TestCase
 {
+    public function tearDown(): void
+    {
+        Mockery::close();
+    }
+
     /**
      * Test function uses a non existent object to test an other object which needs this one.
      */
@@ -31,6 +37,25 @@ class OrderTest extends TestCase
             ->method('charge')
             ->with($this->equalTo(200))
             ->willReturn(true);
+
+        $order = new Order($gateway, 200);
+
+        $this->assertTrue($order->process());
+    }
+
+    /**
+     * Same test previous but with Mockery library.
+     *
+     * @see http://docs.mockery.io/en/latest/index.html
+     */
+    public function testOrderIsProcessedUsingMockery()
+    {
+        $gateway = Mockery::mock('PaymentGateway');
+        $gateway->shouldReceive('charge')
+            ->once()
+            ->with(200)
+            ->andReturn(true)
+        ;
 
         $order = new Order($gateway, 200);
 
