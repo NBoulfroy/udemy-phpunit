3433727703f5817aa56a2be95d83da6b02f433e9
BOULFROY Nicolas <nicolas.boulfroy@hotmail.fr>
2020-07-02 12:53:43 +0200

    TemperatureService added
    Fully implemented

---
+++ src/TemperatureService.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * TemperatureService class.
+ *
+ * @Project : udemy-phpunit
+ * @File    : TemperatureService.php
+ * @Author  : BOULFROY Nicolas
+ * @Create  : 2020/07/02
+ */
+
+class TemperatureService
+{
+    /**
+     * Get the temperature at a specific time.
+     *
+     * @param string $time
+     *
+     * @return int
+     */
+    public function getTemperature(string $time): int
+    {
+        // ...
+    }
+}
