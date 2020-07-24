c172a365a6bb50f01cd197be9d767a56caaa7498
BOULFROY Nicolas <nicolas.boulfroy@hotmail.fr>
2020-07-24 18:01:53 +0200

    MailerTest added
    Basic implementation

---
+++ tdd/tests/MailerTest.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * MailerTest class.
+ *
+ * @Project : udemy-phpunit
+ * @File    : MailerTest.php
+ * @Author  : BOULFROY Nicolas
+ * @Create  : 2020/07/24
+ */
+
+namespace Tdd\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Tdd\Mailer;
+
+class MailerTest extends TestCase
+{
+    // TODO
+}
