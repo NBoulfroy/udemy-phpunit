a2bd3abc41bb7461fd7d38226821f7d7de173a05
BOULFROY Nicolas <nicolas.boulfroy@hotmail.fr>
2020-07-24 17:01:44 +0200

    AbstractPerson class added
    Basic implementation

---
+++ tdd/src/AbstractPerson.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * AbstractPerson abstract class.
+ *
+ * @Project : udemy-phpunit
+ * @File    : AbstractPerson.php
+ * @Author  : Hollingworth Dave
+ * @Create  : ?
+ * @Update  : 2020/07/24
+ */
+
+namespace Tdd;
+
+/**
+ * Person
+ *
+ * An example abstract person class
+ */
+abstract class AbstractPerson
+{
+    /**
+     * Surname
+     * @var string
+     */
+    protected $surname;
+
+    /**
+     * Constructor
+     *
+     * @param string $surname The person's surname
+     *
+     * @return void
+     */
+    public function __construct(string $surname)
+    {
+        $this->surname = $surname;
+    }
+
+    /**
+     * Get the person's title
+     *
+     * @return string
+     */
+    abstract protected function getTitle();
+
+    /**
+     * Get the person's name
+     *
+     * @return string
+     */
+    public function getNameAndTitle()
+    {
+        return $this->getTitle() . ' ' . $this->surname;
+    }
+}
