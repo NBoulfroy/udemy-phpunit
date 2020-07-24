c536b83c152adff30df69201fb9a75651117579a
BOULFROY Nicolas <nicolas.boulfroy@hotmail.fr>
2020-07-24 15:09:29 +0200

    ItemChild class added

---
+++ tdd/src/ItemChild.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ *
+ *
+ * @Project : udemy-phpunit
+ * @File    : ItemChild.php
+ * @Author  : BOULFROY Nicolas
+ * @Create  : 2020/07/24
+ */
+
+namespace Tdd;
+
+class ItemChild extends Item
+{
+    public function getID()
+    {
+        return parent::getID();
+    }
+
+    public function getToken()
+    {
+        return parent::getToken();
+    }
+}
