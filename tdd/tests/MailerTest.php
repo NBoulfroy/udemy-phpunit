3510d95224f14e35dac8c7690472937eec61a1b2
BOULFROY Nicolas <nicolas.boulfroy@hotmail.fr>
2020-09-30 11:09:57 +0200

    MailerTest updated
    testSendMessageReturnsTrue & testInvalidArgumentExceptionIfEmailIsEmpty added

--- tdd/tests/MailerTest.php
+++ tdd/tests/MailerTest.php
@@ -6,14 +6,32 @@
  * @File    : MailerTest.php
  * @Author  : BOULFROY Nicolas
  * @Create  : 2020/07/24
+ * @Update  : 2020/09/30
  */
 
 namespace Tdd\Tests;
 
 use PHPUnit\Framework\TestCase;
 use Tdd\Mailer;
+use \InvalidArgumentException;
 
 class MailerTest extends TestCase
 {
-    // TODO
+    /**
+     * Tests a static method.
+     */
+    public function testSendMessageReturnsTrue()
+    {
+        $this->assertTrue(Mailer::send('toto@gmail.com', 'Hello!'));
+    }
+
+    /**
+     * Provokes an invalidArgumentException with a static method.
+     */
+    public function testInvalidArgumentExceptionIfEmailIsEmpty()
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        Mailer::send('', '');
+    }
 }
